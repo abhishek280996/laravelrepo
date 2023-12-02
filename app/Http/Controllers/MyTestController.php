570d8c8f90e1b99c3da8b1ba202afd79b28f2962
Abhishek V <abhi280996@gmail.com>
2023-12-02 16:51:31 +0530

    File deleted

--- app/Http/Controllers/MyTestController.php
+++ app/Http/Controllers/MyTestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class myTest1Controller extends Controller
+class MyTestController extends Controller
 {
     public function index()
     {
